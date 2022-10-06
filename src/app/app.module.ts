b2077326c34707b46d18dbe5a12d286b21c51215
Long Tran <tranlong.kma@gmail.com>
2022-10-06 21:38:30 +0700

    break component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,13 @@ import {AppComponent} from './app.component';
 import {SharedModule} from "./shared/components/shared.module";
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import {NzMenuModule} from "ng-zorro-antd/menu";
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { WorkComponent } from './components/work/work.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   imports: [
@@ -15,6 +22,13 @@ import {NzMenuModule} from "ng-zorro-antd/menu";
   ],
   declarations: [
     AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    ExperienceComponent,
+    WorkComponent,
+    BlogComponent,
+    ContactComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
